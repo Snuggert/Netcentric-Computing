4304a034dc6792b85e831a1eef2aa5c2744d0b3a
Snuggert <abe.wiersma@hotmail.nl>
2014-06-02 13:02:58 +0200

    cool stuff

--- potent/main.cpp
+++ potent/main.cpp
@@ -1,12 +1,16 @@
 #include "mbed.h"
+#include "HBridge.h"
 
+HBridge hbridge(p18,p15,p22);
 DigitalOut led1(LED1);
 DigitalOut led2(LED2);
 DigitalOut led3(LED3);
 DigitalOut led4(LED4);
-AnalogIn ain(p18);
+AnalogIn ain(p17);
 
 int main() {
+	hbridge.speed(0.7);
+    hbridge.power(true);
     while (1){
         led1 = (ain > 0.2) ? 1 : 0;
         led2 = (ain > 0.4) ? 1 : 0;
