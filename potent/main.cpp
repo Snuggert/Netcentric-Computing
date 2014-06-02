7be59a6c2f299b6f636ef1fa7c3cfba16437c071
Snuggert <abe.wiersma@hotmail.nl>
2014-06-02 13:30:45 +0200

    interfacing

--- potent/main.cpp
+++ potent/main.cpp
@@ -9,12 +9,21 @@ DigitalOut led4(LED4);
 AnalogIn ain(p17);
 
 int main() {
-	hbridge.speed(0.7);
+    float speed;
     hbridge.power(true);
     while (1){
         led1 = (ain > 0.2) ? 1 : 0;
         led2 = (ain > 0.4) ? 1 : 0;
         led3 = (ain > 0.6) ? 1 : 0;
         led4 = (ain > 0.8) ? 1 : 0;
+
+        if(ain<0.45){
+            speed = 1.0;
+        }else if(ain > 0.55){
+            speed = -1.0;
+        }else{
+            speed = 0.0;
+        }
+        hbridge.speed(speed);
     }
 }
