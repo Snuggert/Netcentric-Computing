75eef366e72294b425584fbfe91ee755fb7c3c17
Jelte Fennema <github@jeltef.nl>
2014-06-03 23:45:51 +1000

    Make main.cpp of the unix filetype and add entering of integers instead of voltages

--- potent/main.cpp
+++ potent/main.cpp
@@ -1,4 +1,4 @@
-#include "mbed.h"
+#include "mbed/mbed.h"
 #include "HBridge.h"
 
 #include <string>
@@ -12,38 +12,46 @@ AnalogIn ain(p17);
 Serial pc(USBTX, USBRX);
 
 int main() {
-    float speed, stand = 0.5;
+    float speed;
+    int stand = 5;
     char buf[256];
     char *pend;
     int i;
     char c;
 
+    float voltages[11] = {0.0, 0.008, 0.0325, 0.0975, 0.163, 0.2315, 0.3066,
+         0.4529, 0.7941, 0.999, 1.1};
+
     pend = (char*)malloc(sizeof(char));
     hbridge.power(true);
-    while (1){
-        led1 = (ain > 0.2) ? 1 : 0;
-        led2 = (ain > 0.4) ? 1 : 0;
-        led3 = (ain > 0.6) ? 1 : 0;
-        led4 = (ain > 0.8) ? 1 : 0;
+    while (1) {
+        led1 = (ain > voltages[2]) ? 1 : 0;
+        led2 = (ain > voltages[4]) ? 1 : 0;
+        led3 = (ain > voltages[6]) ? 1 : 0;
+        led4 = (ain > voltages[8]) ? 1 : 0;
 
-        if(ain< stand - 0.05){
+        if (ain< voltages[stand] - 0.0005) {
             speed = 1.0;
-        }else if(ain > stand + 0.05){
+        } else if (ain > voltages[stand] + 0.0005) {
             speed = -1.0;
-        }else{
+        } else {
             speed = 0.0;
         }
         hbridge.speed(speed);
 
-        if(pc.readable()){
+        if (pc.readable()) {
             for (i = 0; i < 255 && (c = pc.getc()) != '\r'; i++) {
                 buf[i] = c;
                 pc.putc(buf[i]);
             }
             buf[i] = '\0';
-            stand = strtof(buf, &pend);
+            stand = (int) strtof(buf, &pend);
 
-            pc.printf("\n\rStand: %f\n\r", stand);
+            if (stand > 10) {
+                stand = 10;
+            }
+
+            pc.printf("\n\rStand: %d\n\r", stand);
         }
     }
 }
