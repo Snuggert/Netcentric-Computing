cd5d012227f6c3d2e3498839edce1e352fe052a1
Snuggert <abe.wiersma@hotmail.nl>
2014-06-05 13:53:39 +0200

    now compilable

--- potent_app/app/src/main/java/com/example/potent_app/app/BluetoothServer.java
+++ potent_app/app/src/main/java/com/example/potent_app/app/BluetoothServer.java
@@ -12,17 +12,16 @@ import java.util.UUID;
 /**
  * Created by root on 5-6-14.
  */
-public class BluetoothServer implements Runnable{
+public class BluetoothServer implements Runnable {
     public static final String PROTOCOL_SCHEME_RFCOMM = "btspp";
 
     BluetoothAdapter mBluetooth = BluetoothAdapter.getDefaultAdapter();
     private BluetoothServerSocket mServerSocket;
 
     // initialization.
-    public BluetoothServer(Context context) throws IOException
-    {
+    public BluetoothServer(Context context) throws IOException {
         if (!mBluetooth.isEnabled()) {
-            throw (new IOException("No mBed attached"));
+            throw (new IOException("No bluetooth enabled."));
         }
     }
 
@@ -32,19 +31,20 @@ public class BluetoothServer implements Runnable{
         BluetoothSocket socket = null;
         try {
             mServerSocket = mBluetooth.listenUsingRfcommWithServiceRecord(PROTOCOL_SCHEME_RFCOMM,
-                   UUID.fromString("a60f35f0-b93a-11de-8a39-08002009c666"));
-        }catch (IOException e) {
+                    UUID.fromString("a60f35f0-b93a-11de-8a39-08002009c666"));
+        } catch (IOException e) {
             Log.e("Server Socket listening failed", "", e);
         }
-        while(true){
+        while (true) {
             try {
                 socket = mServerSocket.accept();
-            }catch (IOException e) {
+            } catch (IOException e) {
                 Log.e("Socket acceptance failed", "", e);
             }
-            if(socket != null) {
+            if (socket != null) {
 
             }
-    }
+        }
 
+    }
 }
